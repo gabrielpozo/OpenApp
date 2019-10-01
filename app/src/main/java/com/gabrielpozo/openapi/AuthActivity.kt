f56c0a056f18b616b9531f31859b8ab81726b1bb
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2019-10-01 13:12:07 +0200

    initial start point

---
+++ app/src/main/java/com/gabrielpozo/openapi/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.gabrielpozo.openapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
