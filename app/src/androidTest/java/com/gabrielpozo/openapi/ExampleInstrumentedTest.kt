b02ac8d3db0c6c9e20ee11eb796ae07f37221053
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2019-10-07 22:15:53 +0200

    package base - structure name changed

--- app/src/androidTest/java/com/gabrielpozo/openapi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gabrielpozo/openapi/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.codingwithmitch.openapi", appContext.packageName)
+        assertEquals("com.gabrielpozo.openapp", appContext.packageName)
     }
 }
